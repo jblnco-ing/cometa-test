f0cc5e9a859b0592a7728a5695fa972473cf8dab
Joel Blanco <jblnco.ing@gmail.com>
2022-05-07 17:27:39 -0400

    refactor: service student

--- src/services/student.ts
+++ src/services/student.ts
@@ -3,10 +3,10 @@ import api from "lib/api";
 
 export const find = async (id: string) => {
   const res = await api.get(`v1/students/${id}`);
-  return await res.data;
+  return res.data;
 };
 
 export const orders = async (id: string) => {
-  const res = await api.get(`v1/students/${id}/orders`);
-  return await res.data;
+  const res = await api.get<any[]>(`v1/students/${id}/orders`);
+  return res.data;
 };
