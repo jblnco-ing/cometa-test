1ed94bd2a87b3606aa9bfaab284a33efff31041b
Joel Blanco <jblnco.ing@gmail.com>
2022-05-07 12:33:25 -0400

    refactor: api.ts and student.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-	headers:{hash: process.env.NEXT_PUBLIC_API_HASH || ''}
+  headers: { hash: process.env.NEXT_PUBLIC_API_HASH || "" },
 });
--- src/services/student.ts
+++ src/services/student.ts
@@ -1,11 +1,12 @@
-import apiService from "./api";
+//lib
+import api from "lib/api";
 
-export const find = async (id:string) => {
-	const res = await apiService.get(`v1/students/${id}`)
-	return await res.data
+export const find = async (id: string) => {
+  const res = await api.get(`v1/students/${id}`);
+  return await res.data;
 };
 
-export const orders = async (id:string) => {
-	const res = await apiService.get(`v1/students/${id}/orders`)
-	return await res.data
+export const orders = async (id: string) => {
+  const res = await api.get(`v1/students/${id}/orders`);
+  return await res.data;
 };
